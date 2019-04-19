3f19e493dffaf29988ad524144533091519e07c0
corget <1608379481@qq.com>
2019-04-19 14:39:53 +0800

    打卡提示改进，集合的排序和去重

--- src/java/com/io/TestObject.java
+++ src/java/com/io/TestObject.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.Serializable;
 
 /**
- * ʱ��:2019��2��22��
  * @author pefuxc
  *
  */
--- src/java/com/testtimer/DaKaTiShi.java
+++ src/java/com/testtimer/DaKaTiShi.java
@@ -11,7 +11,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 /**
- * ʱ��:2019��3��6��
  * @author pefuxc
  *
  */
@@ -19,14 +18,13 @@ public class DaKaTiShi extends TimerTask {
 
 	private Frame aFrame=new Frame();
 	private static long daySpan = 24 * 60 * 60 * 1000;
-	private Button login=new Button("ȷ��");
+	private Button login=new Button("ok");
 	private Label login_clew=new Label();
-	private static String aMorningDate="2019-03-06 09:00:00";
-	private String aNightDate="2019-03-06 09:00:00";
-	private static Date b=null;
-	private static int count=0;
-	private Calendar c = Calendar.getInstance();
+	private static String aMorningDate="2019-04-18 08:20:00";
+	private static String aNightDate="2019-04-18 19:00:00";
+	
 	private static SimpleDateFormat a=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
 	private class DaKaTiShiActionListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -42,12 +40,14 @@ public class DaKaTiShi extends TimerTask {
 		aFrame.setSize(400,300);
 		aFrame.setVisible(true);
 		aFrame.setResizable(false);
-		login.setBounds(90, 200, 60, 30);
+		login.setBounds(140, 200, 60, 30);
 		login.setVisible(true);
 		aFrame.add(login);
 		
-		login_clew.setBounds(90,160,200,30);
-	    login_clew.setText("��Ŷ,�ϵܣ�");
+		login_clew.setBounds(90,100,200,30);
+		login_clew.setBackground(Color.RED);
+		login_clew.setSize(230, 30);
+	    login_clew.setText("Don't forget to punch in even after work.");
 	    aFrame.add(login_clew);
 	    DaKaTiShiActionListener aDaKaTiShiActionListener=new DaKaTiShiActionListener();
 		login.addActionListener(aDaKaTiShiActionListener);
@@ -55,27 +55,21 @@ public class DaKaTiShi extends TimerTask {
 	@Override
 	public void run() {
 		init();
-		//ÿִ����һ��
-        c.setTime(b);
-        c.add(Calendar.DAY_OF_MONTH, 1);// ��
-        b=c.getTime();
-        System.out.println(b.getTime());
 	}
 	public static void main(String[] args) {
+		Date b = null;
+		Date d = null;
 		try {
-			if(count==0){
-			   b=a.parse(aMorningDate);
-			   count=1;
-			}
-			System.out.println(count);
-		} catch (ParseException e) {
-			e.printStackTrace();
+			b = a.parse(aMorningDate);
+			d = a.parse(aNightDate);
+		} catch (ParseException e1) {
+			e1.printStackTrace();
 		}
 		Timer aTimer=new Timer();
-//		Timer aTimer2=new Timer(); 
+		Timer aTimer2=new Timer(); 
 		DaKaTiShi aDaKaTiShi=new DaKaTiShi();
-//		DaKaTiShi aDaKaTiShi2=new DaKaTiShi();
+		DaKaTiShi aDaKaTiShi2=new DaKaTiShi();
 		aTimer.schedule(aDaKaTiShi,b,daySpan);
-//		aTimer2.schedule(aDaKaTiShi2, new Date(),2000*1);
+		aTimer2.schedule(aDaKaTiShi2, d,daySpan);
 	}
 }
